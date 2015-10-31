7a4c705864b15ab59f5654ed549b2b7112f0723d
jimfutsu <jimmyli2002@gmail.com>
2015-10-30 23:58:29 -0700

    Added RCONScript

---
+++ RCONScript/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>A BASIC HTML FORM</title>
+</head>
+<body>
+
+<FORM NAME ="ServInfo" METHOD ="POST" ACTION = "RCONSC.php">
+
+<INPUT TYPE = "TEXT" VALUE ="IP" NAME = "IP">
+<INPUT TYPE = "TEXT" VALUE ="RCONPASS" NAME = "RCONPASS">
+<INPUT TYPE = "TEXT" VALUE ="COMMAND" NAME = "COMMAND">
+<INPUT TYPE = "Submit" Name = "Submit" VALUE = "Send">
+
+  </FORM>
+
+
+
+</body>
+</html>
